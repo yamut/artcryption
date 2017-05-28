49113aebce7d6c60619ab17a16f34f154ae9c856
Dan Mattern <dan@danmattern.com>
2017-05-27 20:01:16 -0400

    unset vars

--- tests/junk.php
+++ tests/junk.php
@@ -4,7 +4,7 @@ require_once( ".." . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . 'Artcry
 
 $a = new Artcryption( 'cat.jpg', 'cat.jpg.encoded.png', Artcryption::ENCODE, 'storage' . DIRECTORY_SEPARATOR );
 $b = new Artcryption( 'cat.jpg.encoded.png', 'cat.jpg.decoded.jpg', Artcryption::DECODE, 'storage' . DIRECTORY_SEPARATOR );
-
+unset( $a, $b );
 $a = new Artcryption( 'foo.txt', 'foo.txt.png', Artcryption::ENCODE, 'storage' . DIRECTORY_SEPARATOR );
 $b = new Artcryption( 'foo.txt.png', 'foo.txt.decoded', Artcryption::DECODE, 'storage' . DIRECTORY_SEPARATOR );
 
